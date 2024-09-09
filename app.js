834e3668e4a7b1ec464064d9dc7b6710e09c9e34
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-09-09 12:57:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const multer = require('multer');
 const mysql = require('mysql2');
 const path = require('path');
 const fs = require('fs');
-
 const https = require('https');
 const http = require('http');  // Para redirigir de HTTP a HTTPS
+
 const app = express();
 
+const jwt = require('jsonwebtoken');
+
+// Clave secreta para JWT
+const secretKey = '6LdUDjMqAAAAABBnVq8EKW_2tv_xCM4mvrDmxhRV';  // Reemplaza con tu clave secreta de reCAPTCHA
 
 // Cargar los certificados SSL
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/programapatenta.com/privkey.pem', 'utf8');
@@ -35,26 +39,6 @@ httpsServer.listen(443, () => {
 });
 
 
-
-
-
-
-
-
-
-
-
-const jwt = require('jsonwebtoken');
-
-
-
-const secretKey = '6LdUDjMqAAAAABBnVq8EKW_2tv_xCM4mvrDmxhRV';  // Reemplaza con tu clave secreta de reCAPTCHA
-
-
-
-
-
-
 // Middleware para verificar el token JWT
 function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization'];
@@ -64,7 +48,7 @@ function verifyToken(req, res, next) {
 
     const token = authHeader.split(' ')[1];  // Extraer el token sin el prefijo 'Bearer'
 
-    jwt.verify(token, secretKey, (err, decoded) => {
+    jwt.verify(token, secretKey, (err, decoded) => {//VERIFICACIÓN DE JWT
         if (err) {
             return res.status(401).json({ success: false, message: 'Token inválido' });
         }
@@ -73,10 +57,8 @@ function verifyToken(req, res, next) {
     });
 }
 
-
-
-
 app.use(express.json());
+
 // Configuración de multer para manejar la carga de archivos
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -88,67 +70,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
-
-// Endpoint protegido para obtener los datos de la tabla postulaciones
-app.get('/postulaciones', verifyToken, (req, res) => {
-    const query = 'SELECT * FROM postulaciones';
-
-    db.query(query, (err, results) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Error al obtener los datos' });
-        }
-        res.json({ success: true, data: results });
-    });
-});
-
-
-
-
-//endpoint para login
-app.post('/login', (req, res) => {
-    const { usuario, contrasena } = req.body;
-    const query = 'SELECT * FROM usuarios WHERE usuario = ?';
-
-    db.query(query, [usuario], (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-            const user = result[0];
-            if (user.contrasena === contrasena) {
-                // Generar el JWT usando la misma clave secreta
-                const token = jwt.sign({ id: user.id, usuario: user.usuario }, secretKey, {
-                    expiresIn: '1h',  // Token válido por 1 hora
-                });
-                return res.json({ success: true, token });
-            } else {
-                return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
-            }
-        } else {
-            return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
-        }
-    });
-});
-
-
-// Endpoint para registro
-app.post('/registro', (req, res) => {
-    console.log('Datos recibidos:', req.body);
-    const { usuario, contrasena, nombre_completo, email } = req.body;
-    
-    const query = 'INSERT INTO usuarios (usuario, contrasena, nombre_completo, email) VALUES (?, ?, ?, ?)';
-
-    db.query(query, [usuario, contrasena, nombre_completo, email], (err, result) => {
-        if (err) {
-            console.error('Error en la consulta:', err);  // Log del error en el servidor
-            return res.status(500).json({ success: false, message: 'Error en el servidor' });  // Enviar un JSON en caso de error
-        }
-        res.json({ success: true });
-    });
-});
-
-
-
-
 
 // Conexión a la base de datos
 const db = mysql.createConnection({
@@ -166,20 +87,56 @@ db.connect((err) => {
 // Configurar el middleware para servir archivos estáticos
 app.use(express.static('public'));
 
-// Endpoint para subir archivos
-const axios = require('axios');
+// Endpoint protegido para obtener los datos de la tabla postulaciones
+app.get('/postulaciones', verifyToken, (req, res) => {
+    const query = 'SELECT * FROM postulaciones';
 
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({ success: false, message: 'Error al obtener los datos' });
+        }
+        res.json({ success: true, data: results });
+    });
+});
 
+//endpoint para login
+app.post('/login', (req, res) => {
+    const { usuario, contrasena } = req.body;
+    const query = 'SELECT * FROM usuarios WHERE usuario = ?';
 
+    db.query(query, [usuario], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            const user = result[0];
+            if (user.contrasena === contrasena) { // Generar el JWT usando la misma clave secreta
+                const token = jwt.sign({ id: user.id, usuario: user.usuario }, secretKey, {//JWT
+                    expiresIn: '1h',  // Token válido por 1 hora
+                });
+                return res.json({ success: true, token });
+            } else {
+                return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
+            }
+        } else {
+            return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
+        }
+    });
+});
 
+// Endpoint para registro
+app.post('/registro', (req, res) => {
+    console.log('Datos recibidos:', req.body);
+    const { usuario, contrasena, nombre_completo, email } = req.body;
+    
+    const query = 'INSERT INTO usuarios (usuario, contrasena, nombre_completo, email) VALUES (?, ?, ?, ?)';
 
-
-
-
-
-
-
-
+    db.query(query, [usuario, contrasena, nombre_completo, email], (err, result) => {
+        if (err) {
+            console.error('Error en la consulta:', err);  // Log del error en el servidor
+            return res.status(500).json({ success: false, message: 'Error en el servidor' });  // Enviar un JSON en caso de error
+        }
+        res.json({ success: true });
+    });
+});
 
 // Endpoint para subir archivos
 app.post('/submit', upload.fields([
@@ -189,34 +146,17 @@ app.post('/submit', upload.fields([
 ]), (req, res) => {
     const { tipo_participante, nombre_institucion, titulo_invencion, sector_tecnologico, apellidos_representante, nombres_representante, dni_representante, correo_electronico, region, recaptcha_token } = req.body;
 
-    // Verificación de reCAPTCHA
-    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha_token}`;
+    const ficha_postulante = req.files['ficha_postulante'] ? req.files['ficha_postulante'][0].path : null;
+    const ficha_invencion = req.files['ficha_invencion'] ? req.files['ficha_invencion'][0].path : null;
+    const acta_compromiso = req.files['acta_compromiso'] ? req.files['acta_compromiso'][0].path : null;
 
-    axios.post(verifyUrl)
-        .then(response => {
-            if (response.data.success) {
-                // Si el reCAPTCHA es válido, continuar con el proceso de inserción en la base de datos
-                const ficha_postulante = req.files['ficha_postulante'] ? req.files['ficha_postulante'][0].path : null;
-                const ficha_invencion = req.files['ficha_invencion'] ? req.files['ficha_invencion'][0].path : null;
-                const acta_compromiso = req.files['acta_compromiso'] ? req.files['acta_compromiso'][0].path : null;
+    const query = 'INSERT INTO postulaciones (tipo_participante, nombre_institucion, titulo_invencion, sector_tecnologico, apellidos_representante, nombres_representante, dni_representante, correo_electronico, region, ficha_postulante, ficha_invencion, acta_compromiso, fecha_creacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())';
 
-                const query = 'INSERT INTO postulaciones (tipo_participante, nombre_institucion, titulo_invencion, sector_tecnologico, apellidos_representante, nombres_representante, dni_representante, correo_electronico, region, ficha_postulante, ficha_invencion, acta_compromiso, fecha_creacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())';
-
-                db.query(query, [tipo_participante, nombre_institucion, titulo_invencion, sector_tecnologico, apellidos_representante, nombres_representante, dni_representante, correo_electronico, region, ficha_postulante, ficha_invencion, acta_compromiso], (err, result) => {
-                    if (err) throw err;
-                    // Enviar una respuesta que el frontend pueda capturar
-                    res.json({ success: true, message: 'Postulación ingresada' });
-                });
-            } else {
-                res.status(400).json({ success: false, message: 'Verificación de reCAPTCHA fallida' });
-            }
-        })
-        .catch(error => {
-            console.error('Error al verificar reCAPTCHA:', error);
-            res.status(500).json({ success: false, message: 'Error en la verificación de reCAPTCHA' });
-        });
+    db.query(query, [tipo_participante, nombre_institucion, titulo_invencion, sector_tecnologico, apellidos_representante, nombres_representante, dni_representante, correo_electronico, region, ficha_postulante, ficha_invencion, acta_compromiso], (err, result) => {
+        if (err) throw err;
+        res.json({ success: true, message: 'Postulación ingresada' });
+    });
 });
-
 
 // Endpoint para obtener un archivo
 app.get('/uploads/:file', (req, res) => {
